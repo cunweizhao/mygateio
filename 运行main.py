5fbc565b5cebdcb631dc20848c95d3580bdbf43e
赵存伟 <17090888178>
2025-08-26 19:53:27 +0800

    运行方法

--- 运行main.py
+++ 运行main.py
@@ -1,1 +1,5 @@
 # python main.py live --symbol ETH_USDT --interval 10s --poll 3 --window 30 --mode both --hold 120 --vol_thr 1.05 --z_thr 1.8 --atr_mult 0.6
+#.\venv\Scripts\python.exe main.py live --symbol ETH_USDT --interval 10s --poll 3 --window 30 --mode both --hold 120 --vol_thr 1.05 --z_thr 1.8 --atr_mult 0.6 *> run.log 2>&1
+#cmd /c python main.py live --symbol ETH_USDT --interval 10s --poll 3 --window 30 --mode both --hold 120 --vol_thr 1.05 --z_thr 1.8 --atr_mult 0.6
+# 正常运行
+# cmd /d /c ".\venv\Scripts\python.exe -u main.py live --symbol ETH_USDT --interval 10s --poll 3 --window 30 --mode both --hold 120 --vol_thr 1.05 --z_thr 1.8 --atr_mult 0.6"
